3b8ff21da913485bc5e3cec5d9c8e1f1d56aa474
alnicolaou96 <alnicolaou96@gmail.com>
2018-07-11 15:47:05 -0400

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+﻿function hover()
+{
+    var x = document.getElementsByClassName("contact-methods")[0];
+    var z = document.getElementsByClassName("contact-methods")[2];
+    if (x.style.fontSize = "64px") {
+        x.style.fontSize = "10px";
+        z.style.fontSize = "10px";
+    }
+    else {
+    x.style.fontSize = "64px";
+    z.style.fontSize = "64px";
+    }
+
+}
